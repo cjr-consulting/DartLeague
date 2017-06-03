88e157ad249f24d54d9c2cff4ad062f6de842f4f
pete christinzio <pete.christinzio@persante.com>
2017-06-03 18:26:21 -0400

    sponsor management views converted to .net

---
+++ src/DartLeague/DartLeague.Web/Areas/Manage/Controllers/SponsorController.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DartLeague.Repositories.LeagueData;
+using DartLeague.Web.Areas.Manage.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace DartLeague.Web.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class SponsorController : Controller
+    {
+        private readonly LeagueContext _leagueContext;
+
+        public SponsorController(LeagueContext leagueContext)
+        {
+            _leagueContext = leagueContext;
+        }
+        public IActionResult Index()
+        {
+            var sponsorsList = new SponsorListViewModel
+            {
+                Sponsors = _leagueContext.Sponsors.Select(x =>
+                        new SponsorViewModel()
+                        {
+                            Name = x.Name,
+                            Type = x.Type,
+                            ContactName = x.ContactName,
+                        })
+                    .ToList()
+            };
+            return View(sponsorsList);
+        }
+
+        public IActionResult Create()
+        {
+
+            ViewBag.SponsorTypes = new List<SelectListItem>
+            {
+                new SelectListItem() {Text = "League Sponsors and Partners",Value = "L"},
+                new SelectListItem() {Text = "Player Companies",Value= "P"},
+                new SelectListItem() {Text = "Charity Partners", Value="C"},
+                new SelectListItem() {Text = "Team Sponsors", Value="T"}
+            };
+
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            ViewBag.SponsorTypes = new List<SelectListItem>
+            {
+                new SelectListItem() {Text = "League Sponsors and Partners",Value = "L"},
+                new SelectListItem() {Text = "Player Companies",Value= "P"},
+                new SelectListItem() {Text = "Charity Partners", Value="C"},
+                new SelectListItem() {Text = "Team Sponsors", Value="T"}
+            };
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(SponsorViewModel sponsor)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(SponsorViewModel sponsor)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var p = await _leagueContext.Sponsors.FirstOrDefaultAsync(x => x.Id == Id);
+            if (p == null)
+                return RedirectToAction("Index");
+
+            _leagueContext.Sponsors.Remove(p);
+            _leagueContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/DartLeague/DartLeague.Web/Areas/Manage/Models/SponsorViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Internal;
+
+namespace DartLeague.Web.Areas.Manage.Models
+{
+    public class SponsorViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Contact Name")]
+        public string ContactName { get; set; }
+
+        public string Type { get; set; }
+
+        [Phone]
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        [DisplayName("Street")]
+        public string Address1 { get; set; }
+
+        [DisplayName("Apt./Bldg.")]
+        public string Address2 { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string Zip { get; set; }
+
+        [DisplayName("Web Site Url")]
+        public string Url { get; set; }
+
+        [DisplayName("Facebook")]
+        [Url]
+        public string FacebookUrl { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DisplayName("Map")]
+        [Url]
+        public string MapUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string Comments { get; set; }
+
+        public IEnumerable<TeamViewModel> Teams{get;set;}
+    }
+
+    public class TeamViewModel
+    {
+    }
+}
