d3a87b8f7b642e940ec271b341c6b9fa0d4f6e96
John Meade <john.meade@cjrconsultingllc.com>
2017-06-03 19:13:38 -0400

    moved identity server configuration into an extension.

---
+++ src/DartLeague/DartLeague.Web/Configurations/IdentityExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DartLeague.Web.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DartLeague.Web.Configurations
+{
+    public static class IdentityExtensions
+    {
+        public static void AddIdentityServerConfiguration(this IServiceCollection services, string authSqlConnectionString, string migrationsAssembly)
+        {
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = false;
+
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+
+                // Cookie settings
+                options.Cookies.ApplicationCookie.ExpireTimeSpan = TimeSpan.FromDays(150);
+                options.Cookies.ApplicationCookie.LoginPath = "/Account/LogIn";
+                options.Cookies.ApplicationCookie.LogoutPath = "/Account/LogOff";
+
+                // User settings
+                options.User.RequireUniqueEmail = true;
+            });
+            
+            services.AddDbContext<AuthDbContext>(options =>
+                options.UseMySql(
+                    authSqlConnectionString,
+                    b => b.MigrationsAssembly(migrationsAssembly)
+                )
+            );
+        }
+    }
+}
