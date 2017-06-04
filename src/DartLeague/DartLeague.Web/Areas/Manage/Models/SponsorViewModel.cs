9c187f461e98b93a36499ec95f39e8bdd1b9d337
Pete Christinzio <peter.christinzio@gmail.com>
2017-06-03 22:14:54 -0400

    sponsor management views converted to .net (#17)
    * sponsor management views converted to .net
    
    * added validate attribute to create and edit methods
    
    changed edit param to int?
    
    added error catch all div for create and edit forms
    
    * Sponsors management working with data
    
    * fixed formating

---
+++ src/DartLeague/DartLeague.Web/Areas/Manage/Models/SponsorViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Internal;
+
+namespace DartLeague.Web.Areas.Manage.Models
+{
+    public class SponsorViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Contact Name")]
+        public string ContactName { get; set; }
+
+        public string Type { get; set; }
+
+        [Phone]
+        [Required]
+        public string Phone { get; set; }
+
+        [DisplayName("Street")]
+        public string Address1 { get; set; }
+
+        [DisplayName("Apt./Bldg.")]
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+
+        [DisplayName("Web Site Url")]
+        public string Url { get; set; }
+
+        [DisplayName("Facebook")]
+        public string FacebookUrl { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DisplayName("Map")]
+        public string MapUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string Comments { get; set; }
+     }
+}
