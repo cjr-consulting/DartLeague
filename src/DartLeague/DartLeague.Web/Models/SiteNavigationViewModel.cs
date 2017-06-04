70856dafea0d32c6baba4db870afb0c1c62886af
John Meade <john.meade@cjrconsultingllc.com>
2017-06-03 21:31:53 -0400

    Updated the Nav Partial to Nav ViewComponent
    This removed the HtmlLeagueHelper that wasn't needed now that it is in a view component.

---
+++ src/DartLeague/DartLeague.Web/Models/SiteNavigationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DartLeague.Web.Models
+{
+    public class SiteNavigationViewModel
+    {
+        public List<NavigationViewModel> ParentNavigations { get; set; } = new List<NavigationViewModel>();
+    }
+
+    public class NavigationViewModel
+    {
+        public string Title { get; set; }
+        public string Href { get; set; } = "#";
+        public bool IsHeader { get; set; }
+        public bool IsSeperator { get; set; }
+        public List<NavigationViewModel> SubNavigations { get; set; } = new List<NavigationViewModel>();
+    }
+}
