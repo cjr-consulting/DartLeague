4af62e2223096859a434d872740e114dc53d2c9a
John Meade <john.meade@cjrconsultingllc.com>
2017-06-24 14:58:59 -0400

    initial attempt for a season management layout

---
+++ src/DartLeague/DartLeague.Domain/Season/SeasonManagementService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DartLeague.Domain.Season
+{
+    public class SeasonManagementService
+    {
+        public SeasonManagementService()
+        {
+
+        }
+        public List<LeagueSeason> GetAllSeason()
+        {
+
+            return new List<LeagueSeason>();
+        }
+    }
+}
---
+++ src/DartLeague/DartLeague.Web/Areas/Manage/Controllers/SeasonController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DartLeague.Web.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class SeasonController : Controller
+    {
+        [Route("manage/season/{id}")]
+        public IActionResult Index(int id)
+        {
+            ViewData["SeasonNavPage"] = "Links";
+            return View();
+        }
+
+        [Route("manage/season/{id}/links")]
+        public IActionResult Links(int id)
+        {
+            ViewData["SeasonNavPage"] = "Links";
+            return View();
+        }
+
+        [Route("manage/season/{id}/messages")]
+        public IActionResult Messages(int id)
+        {
+            ViewData["SeasonNavPage"] = "Messages";
+            return View();
+        }
+
+        [Route("manage/season/{id}/competitions")]
+        public IActionResult Competitions(int id)
+        {
+            ViewData["SeasonNavPage"] = "Competitions";
+            return View();
+        }
+    }
+}
