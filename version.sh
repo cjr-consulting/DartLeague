45623044a740236525e390fce1659b4c4d83f1ed
John Meade <johnnynibbles@gmail.com>
2017-08-01 23:38:38 -0400

    More comments in version.sh

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@ export SEMVER_LAST_TAG=$(git describe --abbrev=0 --tags 2>/dev/null)
 export SEMVER_RELEASE_LEVEL=$(git log --oneline -1 --pretty=%B | cat | tr -d '\n' | cut -d "[" -f2 | cut -d "]" -f1)
 
 if [ -z $SEMVER_LAST_TAG ]; then
-    export SEMVER_LAST_TAG=$(0.0.1)
+    export SEMVER_LAST_TAG="0.0.1"
     echo "No tags defined"
 fi
 
@@ -17,9 +17,11 @@ $(cd /tmp/semver; git checkout tags/1.2.1)
 export PATH=$PATH:/tmp/semver/src
 semver init $SEMVER_LAST_TAG &>/dev/null
 
-if [ -n $SEMVER_RELEASE_LEVEL ]; then
+if [ -n "$SEMVER_RELEASE_LEVEL" ]; then
+    echo "Release Level: FOUND"
     semver bump $SEMVER_RELEASE_LEVEL &>/dev/null
 else
+    echo "Release Level: NONE"
     semver bump minor &>/dev/null
 fi
 
