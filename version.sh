c61ad8dac56fdf35caffbe1e0d7c7f4829b92857
John Meade <johnnynibbles@gmail.com>
2017-08-02 13:11:44 -0400

    Wrapped Version.sh variables in quotes

--- version.sh
+++ version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export SEMVER_LAST_TAG=$(git describe --abbrev=0 --tags 2>/dev/null)
-export SEMVER_RELEASE_LEVEL=$(git log --oneline -1 --pretty=%B | cat | tr -d '\n' | cut -d "[" -f2 | cut -d "]" -f1)
+export SEMVER_LAST_TAG="$(git describe --abbrev=0 --tags 2>/dev/null)"
+export SEMVER_RELEASE_LEVEL="$(git log --oneline -1 --pretty=%B | cat | tr -d '\n' | cut -d "[" -f2 | cut -d "]" -f1)"
 
 if [ -z $SEMVER_LAST_TAG ]; then
     export SEMVER_LAST_TAG="0.0.1"
@@ -25,4 +25,4 @@ else
     semver bump minor &>/dev/null
 fi
 
-export SEMVER_NEXT=$(semver)
+export SEMVER_NEXT="$(semver)"
