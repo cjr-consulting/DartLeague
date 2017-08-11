f2a960cbb714fa1b50f146a4efd283271eaee668
John Meade <johnnynibbles@gmail.com>
2017-08-11 17:36:36 -0400

    Added a manager role to the initialization script

--- src/DartLeague/DartLeague.Web/Data/Initializers/InitializeAuthDb.cs
+++ src/DartLeague/DartLeague.Web/Data/Initializers/InitializeAuthDb.cs
@@ -30,6 +30,15 @@ namespace DartLeague.Web.Data.Initializers
 
                     //await roleStore.AddClaimAsync(adminRole, new Claim(CustomClaimTypes.Permission, "account.manage"));
                 }
+
+
+                if (!context.Roles.Any(r => r.Name == "Manager"))
+                {
+                    var userRole = new IdentityRole("Manager");
+                    await roleStore.CreateAsync(userRole);
+
+                    //await roleStore.AddClaimAsync(adminRole, new Claim(CustomClaimTypes.Permission, "account.manage"));
+                }
             }
         }
     }
