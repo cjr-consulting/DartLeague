182404d7bc7b7af3b7b4a9e9b20e79accc94c94b
John Meade <johnnynibbles@gmail.com>
2018-07-29 19:28:34 -0400

    Add project for teamcompetition library

---
+++ src/DartLeague/DartLeague.GameEvents.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DartLeague.GameEvents.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/DartLeague/DartLeague.GameEvents/TeamEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DartLeague.GameEvents
+{
+    class TeamEvent
+    {
+    }
+}
