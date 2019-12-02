1f1b1f5f09405e45102e3f113e8f5e3064adff6a
John Meade <johnnynibbles@gmail.com>
2019-12-01 22:26:11 -0500

    add first stage of game event domain

---
+++ src/DartLeague/DartLeague.GameEvents/GameEvent.cs
@@ -0,0 +1,101 @@
+﻿using DartLeague.Common.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DartLeague.GameEvents
+{
+    public class GameEvent
+    {
+        private List<Schedule> schedules = new List<Schedule>();
+        private List<Team> teams = new List<Team>();
+
+        public void AddTeam(Team team)
+        {
+            teams.Add(team);
+        }
+    }
+
+
+    public class Team : Entity<int>
+    {
+        public string Name { get; }
+        public Venue HomeVenue { get; }
+
+        public Team(int id, string name, Venue homeVenue)
+        {
+            Id = id;
+            Name = name;
+            HomeVenue = homeVenue;
+        }
+    }
+
+    public class Venue
+    {
+        public string Name { get; }
+        public StreetAddress Address { get; }
+
+        public Venue(string name, StreetAddress address)
+        {
+            Name = name;
+            Address = address;
+        }
+    }
+
+    public class StreetAddress
+    {
+        public string Street { get; }
+        public string Street2 { get; }
+        public string City { get; }
+        public string State { get; }
+        public string Zip { get; }
+        public StreetAddress(string street, string street2, string city, string state, string zip)
+        {
+            Street = street;
+            Street2 = street2;
+            City = city;
+            State = state;
+            Zip = zip;
+        }
+    }
+
+    public class Player
+    {
+        public string Name { get; }
+
+    }
+
+    public class Schedule
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int SortId { get; set; }
+    }
+
+    public class WeeklySchedule
+    {
+        public string DayOfWeek { get; set; }
+
+        private List<ScheduledMatch> matches = new List<ScheduledMatch>();
+
+        public void AddMatch(ScheduledMatch match)
+        {
+            matches.Add(match);
+        }
+    }
+
+    public class ScheduledMatch
+    {
+        public DateTime Date { get; }
+        public Team HomeTeam { get; }
+        public Team AwayTeam { get; }
+        public Venue Venue { get; }
+
+        public MatchRule Rules { get; }
+    }
+
+    public class MatchRule
+    {
+        public string Name { get; }
+    }
+}
